7401742e9c5b36f44e08356d0bfb1ec803cdb9b8
Antonio Oliva Carceles <116766832+olixva@users.noreply.github.com>
2025-11-29 17:29:16 +0100

    feat: add user_wallet field to User model

--- app/models/user.py
+++ app/models/user.py
@@ -1,12 +1,19 @@
 from beanie import Document
-from pydantic import EmailStr
 from datetime import datetime
+from pydantic import BaseModel, EmailStr
+
+
+class UserWallet(BaseModel):
+    bsv_address: str
+    bsv_pubkey: str
+    encrypted_wif: str
 
 
 class User(Document):
     name: str
     email: EmailStr
     created_at: datetime
+    user_wallet: UserWallet
 
     class Settings:
         name = "users"
