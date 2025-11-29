0d752f465f0faf190e7a33a812cbbccdfda8ebef
Néstor Villa <nestorvilla@patitasco.com>
2025-11-29 20:15:57 +0100

    fix: update CORS middleware to allow requests from localhost:5173

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
+        "http://localhost:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
