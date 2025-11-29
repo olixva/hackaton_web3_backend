67870256b9b989a2739c56a4b3eddc1213d91e0b
Antonio Oliva Carceles <116766832+olixva@users.noreply.github.com>
2025-11-29 21:56:56 +0100

    feat: add optional profile_image_url field to CreateUserRequest model

--- app/dtos/user/user_request.py
+++ app/dtos/user/user_request.py
@@ -4,12 +4,14 @@ from pydantic import BaseModel, EmailStr, ConfigDict
 class CreateUserRequest(BaseModel):
     name: str
     email: EmailStr
+    profile_image_url : str | None = None
 
     model_config = ConfigDict(
         json_schema_extra={
             "example": {
                 "name": "John Doe",
                 "email": "john.doe@example.com",
+                "profile_image_url": "https://example.com/profile.jpg"
             }
         }
     )
