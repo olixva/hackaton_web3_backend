ab3f70e1e2fdf4fd6c155919fd9043b2501d5cd3
Néstor Villa <nestorvilla@patitasco.com>
2025-11-29 18:31:36 +0100

    feat: implement user creation functionality

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,9 +1,13 @@
+from datetime import datetime
 from fastapi import HTTPException
 
+# DTOs
 from app.dtos.user.user_response import GetUserResponse
+from app.dtos.user.user_response import CreateUserResponse
+# Models
+from app.models.user import UserWallet
 from app.models.user import User
 from beanie import PydanticObjectId
-
 
 
 class UserService:
@@ -19,3 +23,20 @@ class UserService:
 
         return GetUserResponse(id=str(user.id), name=user.name)
     
+    @staticmethod
+    async def create_user(name: str, email: str) -> CreateUserResponse:
+        # TODO: Call wallet generation service
+
+        new_user = User(
+            name=name,
+            email=email,
+            created_at=datetime.now(),
+            user_wallet=UserWallet(
+                bsv_address="",
+                bsv_pubkey="",
+                encrypted_wif="",
+            ),
+        )
+        await new_user.insert()
+
+        return CreateUserResponse(id=str(new_user.id))
