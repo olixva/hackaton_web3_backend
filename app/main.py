e988b9e32671043e20c2b520be82096b6189c519
Néstor Villa <nestorvilla@patitasco.com>
2025-11-29 18:09:28 +0100

    feat: implement lifespan management for FastAPI with MongoDB integration

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 # Routers
 from app.routes.user_route import user_router
 
-app = FastAPI()
+# Database
+from app.config.mongo import MongoDbClient
+
+mongo_client = MongoDbClient()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup actions
+    print("Starting up...")
+    await mongo_client.init()
+    yield
+    # Shutdown actions
+    await mongo_client.close()
+    print("Shutting down...")
+
+app = FastAPI(lifespan=lifespan)
+
 
 app.include_router(user_router)
