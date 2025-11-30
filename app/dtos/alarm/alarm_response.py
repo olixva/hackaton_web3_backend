98f7f4a5dcbba250ba5bcd468babcbe1bb8b1b08
Néstor Villa <nestorvilla@patitasco.com>
2025-11-30 02:31:14 +0100

    fix: add GetAlarmResponse model with example data for improved API response structure

--- app/dtos/alarm/alarm_response.py
+++ app/dtos/alarm/alarm_response.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel, ConfigDict
 from datetime import datetime
 
+from app.models.alarm import AlarmType
+
 
 class CreateAlarmResponse(BaseModel):
     id: str
@@ -9,6 +11,26 @@ class CreateAlarmResponse(BaseModel):
         json_schema_extra={
             "example": {
                 "id": "692b566e0ef3a85601b288f2"
+            }
+        }
+    )
+
+
+class GetAlarmResponse(BaseModel):
+    id: str
+    user_id: str
+    type: AlarmType
+    threshold: float
+    active: bool
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "id": "692b566e0ef3a85601b288f2",
+                "user_id": "692b566e0ef3a85601b288f2",
+                "type": "money",
+                "threshold": 100.0,
+                "active": True,
             }
         }
     )
